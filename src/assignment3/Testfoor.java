444231e905d52508c7a3770a911686e0cdedf4cb
Mohammad Hasan <rakibcse05@gmail.com>
2021-11-09 20:36:18 -0500

    testing some comment

--- src/assignment3/Testfoor.java
+++ src/assignment3/Testfoor.java
@@ -7,6 +7,7 @@ public class Testfoor{
 	      float f = 90;    
 	      double x = 1.234;
 	      double y = Math.round(d * 100.0) / 100.0;
+		  System.out.println("this is for testing line");
 	      /*System.out.println(Math.floor(d));
 	      System.out.println(Math.floor(f)); 
 
